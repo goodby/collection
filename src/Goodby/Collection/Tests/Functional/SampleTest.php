09864b2dc9815f30d576277e7547477d090d5050
suin <suinyeze@gmail.com>
2013-04-01 08:57:36 +0900

    set up project

---
+++ src/Goodby/Collection/Tests/Functional/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Goodby\Collection\Tests\Functional;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(false);
+    }
+}
---
+++ src/Goodby/Collection/Tests/Unit/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Goodby\Collection\Tests\Unit;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(false);
+    }
+}
